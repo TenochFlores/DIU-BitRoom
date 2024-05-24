0c899971e1ddff87fe43e4f93c720a30a4d6394e
TenochFlores <75498092+TenochFlores@users.noreply.github.com>
2024-05-23 22:55:52 -0600

    Estructura base

---
+++ BitRoom/src/app/module/proyecto/component/recursos/audios/audios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audios',
+  templateUrl: './audios.component.html',
+  styleUrl: './audios.component.css'
+})
+export class AudiosComponent {
+
+}
---
+++ BitRoom/src/app/module/proyecto/component/recursos/recursos-de-estudio/recursos-de-estudio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recursos-de-estudio',
+  templateUrl: './recursos-de-estudio.component.html',
+  styleUrl: './recursos-de-estudio.component.css'
+})
+export class RecursosDeEstudioComponent {
+
+}
